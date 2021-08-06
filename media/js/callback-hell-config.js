74306e21382afad5c4f5e5e874e4504ccf342933
Jiří <satorajirka@seznam.cz>
2021-08-06 18:29:53 +0200

    write Callback Hell article

--- media/js/callback-hell-config.js
+++ media/js/callback-hell-config.js
@@ -3,7 +3,6 @@ const codeWithConsoleConfig = [
         elementId: "Example-Alert",
         consoleElement: document.getElementById("Example-Alert__Console"),
         func: function() {
-            console.log(this);
             let text = "&gt; první<br>";
             this.consoleElement.innerHTML = text + "&gt;";
 
@@ -13,6 +12,40 @@ const codeWithConsoleConfig = [
             setTimeout(() => {
                 this.consoleElement.innerHTML = text + "&gt;";
             }, 100);
+        }
+    },
+    {
+        elementId: "Example-SetTimeout",
+        consoleElement: document.getElementById("Example-SetTimeout__Console"),
+        func: function() {
+            let text = "&gt; první<br>";
+            this.consoleElement.innerHTML = text + "&gt;";
+
+            setTimeout(() => {
+                text += "&gt; třetí<br>";
+                this.consoleElement.innerHTML = text + "&gt;";
+            }, 1000);
+
+            text += "&gt; druhý<br>";
+            this.consoleElement.innerHTML = text + "&gt;";
+        }
+    },
+    {
+        elementId: "Example-Callback-Hell",
+        consoleElement: document.getElementById("Example-Callback-Hell__Console"),
+        func: function() {
+            setTimeout(() => {
+                let text = "&gt; první<br>";
+                this.consoleElement.innerHTML = text + "&gt;";
+                setTimeout(() => {
+                    text += "&gt; druhý<br>";
+                    this.consoleElement.innerHTML = text + "&gt;";
+                    setTimeout(() => {
+                        text += "&gt; třetí<br>";
+                        this.consoleElement.innerHTML = text + "&gt;";
+                    }, 1000);
+                }, 1000);
+            }, 1000);
         }
     }
 ];
